fd7e141e4e437f78e574485364889e2efae344db
daskiro514 <luckybuzstar@gmail.com>
2022-03-22 16:29:12 +0900

    updated

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -9,6 +9,23 @@ const Footer = () => {
           <div className='col-12 p-4 text-white text-center'>
             <div className='h1 font-weight-bold'>David Guetta</div>
             <div>Copyright © 2018 What A Music / Powered by SoBuzzee</div>
+            <div className='mt-3'>
+              <div className='mx-2 d-inline-block'>
+                <i className='fa fa-twitter h4 text-secondary'></i>
+              </div>
+              <div className='mx-2 d-inline-block'>
+                <i className='fa fa-facebook h4 text-secondary'></i>
+              </div>
+              <div className='mx-2 d-inline-block'>
+                <i className='fa fa-instagram h4 text-secondary'></i>
+              </div>
+              <div className='mx-2 d-inline-block'>
+                <i className='fa fa-youtube h4 text-secondary'></i>
+              </div>
+              <div className='mx-2 d-inline-block'>
+                <i className='fa fa-spotify h4 text-secondary'></i>
+              </div>
+            </div>
           </div>
         </div>
       </div>
